b78249d3a9bd1e05c65d4df8ac98e06559c2c86c
Kaushik Kanjariya <kaushikkanjariya0111@gmail.com>
2025-07-21 11:57:06 +0530

    Fix import case for Menu component

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,29 @@
 import { createRoot } from 'react-dom/client'
-import Home from './home'
+// import Home from './Home' // Remove if not used
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/Style.css';
 import React from "react";
 import { Dashbord } from './Dashbord';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Menu } from './menu';
+import { Menu } from './Menu'; // <-- Fixed case
 import { Login } from './Login';
 import { Contact } from './Contact';
 import { About } from './About';
 import { Error404 } from './Error404';
 import Result from './Result';
-createRoot(document.getElementById('root')).render(
 
+createRoot(document.getElementById('root')).render(
     <>
         <BrowserRouter>
             <Menu />
             <Routes>
-            <Route path='/' element={<Dashbord/>}></Route>
+                <Route path='/' element={<Dashbord/>}></Route>
                 <Route path='/login' element={<Login/>}></Route>
                 <Route path='/contact' element={<Contact/>}></Route>
                 <Route path='/about' element={<About/>}></Route>
                 <Route path='/result/:fname/:mobile/:email/:password' element={<Result/>}></Route>
                 <Route path='*' element={<Error404/>}></Route>
-                
-
             </Routes>
-
         </BrowserRouter>
-
-
     </>
 )
